1c10b82c39c4477a21bc56e7db1c13232b14e7db
Allan Denot <adenot@gmail.com>
2019-10-26 21:05:32 +1100

    Initial commit - resources split from ecs-app

---
+++ README.md
@@ -0,0 +1,17 @@
+# terraform-aws-ecs-app-front
+
+AWS ECS Application Module (frontend component)
+
+This module is designed to be used with `DNXLabs/terraform-aws-ecs` and `DNXLabs/terraform-aws-ecs-app`.
+
+## Inputs
+
+
+
+## Authors
+
+Module managed by [Allan Denot](https://github.com/adenot).
+
+## License
+
+Apache 2 Licensed. See LICENSE for full details.
---
+++ route53-record.tf
@@ -0,0 +1,21 @@
+data "aws_route53_zone" "selected" {
+  name = var.hosted_zone
+}
+
+resource "aws_route53_record" "hostname" {
+  count   = var.hostname_create ? 1 : 0
+
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.hostname
+  type    = "CNAME"
+  ttl     = "300"
+  records = list(element(aws_cloudfront_distribution.default.*.domain_name, 0))
+}
+
+resource "aws_route53_record" "hostname_blue" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.hostname_blue
+  type    = "CNAME"
+  ttl     = "300"
+  records = list(element(aws_cloudfront_distribution.default.*.domain_name, 0))
+}
