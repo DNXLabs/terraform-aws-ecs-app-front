002b736f081a0d3de25645db20ee0af68f80c41b
Elliot-dnx <maicon.rocha@dnx.solutions>
2020-03-26 18:07:59 +1100

    removing DNS record for blue

--- route53-record.tf
+++ route53-record.tf
@@ -12,12 +12,3 @@ resource "aws_route53_record" "hostname" {
   records = list(element(aws_cloudfront_distribution.default.*.domain_name, 0))
 }
 
-resource "aws_route53_record" "hostname_blue" {
-  count = var.hostname_create ? 1 : 0
-
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = var.hostname_blue
-  type    = "CNAME"
-  ttl     = "300"
-  records = list(element(aws_cloudfront_distribution.default.*.domain_name, 0))
-}
