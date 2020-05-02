392ddc18d9b51aff2654fca26b0796e16b898624
Allan Denot <adenot@gmail.com>
2020-05-02 17:46:13 +1000

    Renaming alarm for errors to describe which error it tracks

--- cloudwatch-alarms.tf
+++ cloudwatch-alarms.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_metric_alarm" "cloudfront_500_errors" {
   provider = aws.us-east-1
   count    = length(var.alarm_sns_topics_us) > 0 && var.alarm_cloudfront_500_errors_threshold != 0 ? 1 : 0
 
-  alarm_name                = "${data.aws_iam_account_alias.current.account_alias}-ecs-${var.cluster_name}-${var.name}-cloudfront-errors"
+  alarm_name                = "${data.aws_iam_account_alias.current.account_alias}-ecs-${var.cluster_name}-${var.name}-cloudfront-500-errors"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "3"
   metric_name               = "5xxErrorRate"
